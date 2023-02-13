3aba0d006f58a0d9f91dcb74d9ac47bb1f7a5bf1
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-02-13 12:21:45 +0600

    src

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,11 @@
 import 'package:ngdart/angular.dart';
-import 'package:tour_of_heroes/hero.dart';
+import 'package:tour_of_heroes/src/hero.dart';
+import 'package:ngforms/ngforms.dart';
 
 @Component(
   selector: 'my-app',
-  template: 'app_component.html',
+  templateUrl: 'app_component.html',
+  directives: [formDirectives]
 )
 class AppComponent {
   final title = 'Tour of Heroes';
